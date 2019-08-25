4183f749b7ceb04f2bb19b709f497784d511b0a0
aldoooyyy <aldifirmansyh@gmail.com>
2019-08-26 01:00:31 +0700

    [UPDATE] Bar chart using SVG

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const graphWidth = 600;
+const graphHeight = 200;
+const padding = 5;
+const svg = d3.select("#graph");
+d3.select("#graph").attr("height", graphHeight);
+d3.select("#graph").attr("width", graphWidth);
+
 d3.select("#enter-btn").on("click", () => {
 	generate();
 });
@@ -21,23 +28,42 @@ function setLetter(letter) {
 }
 
 function setGraphAndCount(letter) {
-	let data = d3.select("#graph")
-			     .selectAll(".bar")
-			     .data(getFrequencies(letter), d => d.character);
+	let frequencies = getFrequencies(letter);
+	let maxFrequency = getMaxFreq(frequencies);
+	let currGraphHeight = maxFrequency > 8 ? graphHeight + 20 * (maxFrequency - 8) : graphHeight;
+	svg.attr("height", currGraphHeight);
+	let data = svg
+			     .selectAll("g")
+			     .data(frequencies, d => d.character);
 
 	data
 			.classed("new", false)
 		.exit()
 		.remove();
 
-	data
+	let barWidth = graphWidth / frequencies.length - padding;
+
+	let letterEnter = data
 	  .enter()
-	  .append("div")
-		  .classed("bar", true)
-		  .classed("new", true)
-		  .text(d => d.character)
-		.merge(data)
-		  .style("height", d => d.count * 20 + "px");
+	  .append("g")
+	  .classed("new", true);
+
+	letterEnter.append("rect");
+	letterEnter.append("text");
+
+	letterEnter.merge(data)
+		.select("rect")
+		  .attr("x", (d, i) => (barWidth + padding) * i)
+		  .attr("y", d => currGraphHeight - d.count * 20)
+		  .attr("height", d => d.count * 20)
+		  .attr("width", barWidth);
+
+	letterEnter.merge(data)
+		.select("text")
+			.text(d => d.character)
+			.attr("x", (d,i) => (barWidth + padding) * i + barWidth / 2)
+			.attr("y", d => currGraphHeight - (d.count * 20 + 10))
+			.attr("text-anchor", "middle");
 
 	d3.select("#count").text("(New Character: " + data.enter().nodes().length + ")");
 }
@@ -54,6 +80,13 @@ function getFrequencies(letter) {
 	}, []);
 }
 
+function getMaxFreq(frequencies) {
+	return frequencies.reduce((acc, next) => {
+		if (next.count > acc) acc = next.count;
+		return acc;
+	}, 0);
+}
+
 d3.select("#reset-btn").on("click", () => {
 	clear();
 });
@@ -61,5 +94,5 @@ d3.select("#reset-btn").on("click", () => {
 function clear() {
 	d3.select("#letter").text("");
 	d3.select("#count").text("");
-	d3.selectAll(".bar").remove();
+	d3.selectAll("rect").remove();
 }
